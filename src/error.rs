c6b7e003414e3ce426326dcdf5de8d917d0ff48e
xgroleau🐢 <xavgroleau@gmail.com>
2022-04-26 19:51:05 -0400

    Revert to i2c only

--- src/error.rs
+++ src/error.rs
@@ -1,21 +1,6 @@
 /// Possible error when reading an register error
 #[derive(Debug, Clone, Copy)]
-pub enum RegisterError<I2CError> {
+pub enum Error<I2CError> {
     /// Internal i2c error
     I2c(I2CError),
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum StuffError<LDOError, DCDCError, RSTError, INTError> {
-    /// Internal HOLD_LDO pin error
-    LDO(LDOError),
-
-    /// Internal HOLD_DCDC pin error
-    DCDC(DCDCError),
-
-    /// Internal reset pin error
-    Reset(RSTError),
-
-    /// Internal interrupt pin error
-    Interrupt(INTError),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,80 +4,58 @@ pub mod error;
 mod registers;
 
 use embedded_hal::{
-    digital::blocking::{InputPin, OutputPin},
     i2c::{blocking::I2c, SevenBitAddress},
 };
-use error::RegisterError;
+use error::Error;
 use registers::*;
 
 const TPS_ADDRESS: u8 = 0b100_1000;
 
-pub struct TPS65720<I2C, I2CE, LDO, LDOE, DCDC, DCDCE, RST, RSTE, INT, INTE>
+pub struct TPS6572x<I2C, I2CE>
 where
     I2C: I2c<SevenBitAddress, Error = I2CE>,
-    LDO: OutputPin<Error = LDOE>,
-    DCDC: OutputPin<Error = DCDCE>,
-    RST: InputPin<Error = RSTE>,
-    INT: InputPin<Error = INTE>,
 {
     i2c: I2C,
-    hold_ldo: LDO,
-    hold_dcdc: DCDC,
-    reset: RST,
-    interrupt: INT,
 }
 
-impl<I2C, I2CE, LDO, LDOE, DCDC, DCDCE, RST, RSTE, INT, INTE>
-    TPS65720<I2C, I2CE, LDO, LDOE, DCDC, DCDCE, RST, RSTE, INT, INTE>
+impl<I2C, I2CE> TPS6572x<I2C, I2CE>
 where
     I2C: I2c<SevenBitAddress, Error = I2CE>,
-    LDO: OutputPin<Error = LDOE>,
-    DCDC: OutputPin<Error = DCDCE>,
-    RST: InputPin<Error = RSTE>,
-    INT: InputPin<Error = INTE>,
 {
-    pub fn new(i2c: I2C, hold_ldo: LDO, hold_dcdc: DCDC, reset: RST, interrupt: INT) -> Self {
-        Self {
-            i2c,
-            hold_ldo,
-            hold_dcdc,
-            reset,
-            interrupt,
-        }
+    pub fn new(i2c: I2C) -> Self {
+        Self { i2c }
     }
     pub fn release(self) -> I2C {
         self.i2c
     }
 
-    pub fn write_register<R>(&mut self, register: R) -> Result<(), RegisterError<I2CE>>
+    pub fn write_register<R>(&mut self, register: R) -> Result<(), Error<I2CE>>
     where
         R: WritableRegister,
         u8: From<R>,
     {
         self.i2c
             .write(TPS_ADDRESS, &[R::ADDRESS as u8])
-            .map_err(RegisterError::I2c)?;
+            .map_err(Error::I2c)?;
         self.i2c
             .write(TPS_ADDRESS, &[register.into()])
-            .map_err(RegisterError::I2c)?;
+            .map_err(Error::I2c)?;
         Ok(())
     }
 
-    pub fn read_register<R>(&mut self) -> Result<R, RegisterError<I2CE>>
+    pub fn read_register<R>(&mut self) -> Result<R, Error<I2CE>>
     where
         R: Register + From<u8>,
     {
         let mut val: [u8; 1] = [0; 1];
         self.i2c
             .write(TPS_ADDRESS, &[R::ADDRESS as u8])
-            .map_err(RegisterError::I2c)?;
-        self.i2c
-            .write(TPS_ADDRESS, &mut val)
-            .map_err(RegisterError::I2c)?;
+            .map_err(Error::I2c)?;
+        self.i2c.write(TPS_ADDRESS, &mut val).map_err(Error::I2c)?;
         Ok(val[0].into())
     }
 
-    pub fn edit_register<R, F>(&mut self, f: F) -> Result<(), RegisterError<I2CE>>
+    pub fn edit_register<R, F>(&mut self, f: F) -> Result<(), Error<I2CE>>
     where
         F: FnOnce(R) -> R,
         R: WritableRegister + From<u8>,
